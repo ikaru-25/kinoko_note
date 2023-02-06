0b4d4f682d9a1bfb92b7f904fde43b2d3805b89d
Ito Kaoru <75163339+ikaru-25@users.noreply.github.com>
2023-02-06 22:50:01 +0900

    観察項目のシートをコンポーネント化

---
+++ lib/components/bottom_sheet.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+Widget builderBottomSheet(context) {
+  return Container(
+    padding: EdgeInsets.only(
+      top: 15,
+      left: 15,
+      right: 15,
+      // this will prevent the soft keyboard from covering the text fields
+      bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+    ),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        TextField(
+          // controller: _fieldNameController,
+          decoration: const InputDecoration(hintText: 'キノコの名前'),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          width: double.infinity,
+          child: Text(
+            "発生場所",
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              children: [
+                Radio(
+                  value: 0,
+                  groupValue: 0,
+                  onChanged: (value) {},
+                ),
+                Text('枯木/倒木')
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: 0,
+                  onChanged: (value) {},
+                ),
+                Text('草地')
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 2,
+                  groupValue: 0,
+                  onChanged: (value) {},
+                ),
+                Text('土')
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 3,
+                  groupValue: 0,
+                  onChanged: (value) {},
+                ),
+                Text('その他')
+              ],
+            ),
+          ],
+        ),
+        // Row(
+        //   children: [],
+        // ),
+        CheckboxListTile(
+          // tileColor: Colors.red,
+          controlAffinity: ListTileControlAffinity.leading,
+          title: const Text('つば'),
+          value: true,
+          onChanged: (bool? value) {},
+        ),
+        CheckboxListTile(
+          controlAffinity: ListTileControlAffinity.leading,
+          // tileColor: Colors.red,
+          title: const Text('つぼ'),
+          value: true,
+          onChanged: (bool? value) {},
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+
+        Container(
+          width: double.infinity,
+          child: Text(
+            "傘の下",
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              children: [
+                Radio(
+                  value: 0,
+                  groupValue: 1,
+                  onChanged: (value) {},
+                ),
+                Text('ひだ')
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: 1,
+                  onChanged: (value) {},
+                ),
+                Text('しわひだ')
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 2,
+                  groupValue: 0,
+                  onChanged: (value) {},
+                ),
+                Text('管孔')
+              ],
+            ),
+            Row(
+              children: [
+                Radio(
+                  value: 3,
+                  groupValue: 0,
+                  onChanged: (value) {},
+                ),
+                Text('針')
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            // Navigator.of(context).pop();
+          },
+          child: Text('保存'),
+        )
+      ],
+    ),
+  );
+}
