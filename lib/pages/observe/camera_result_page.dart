4aba05caac8f0959b1fb5b09d1a7a93661351fdb
Ito Kaoru <75163339+ikaru-25@users.noreply.github.com>
2023-02-14 23:03:04 +0900

    画像スライドの実装
    雛形を作成

--- lib/pages/observe/camera_result_page.dart
+++ lib/pages/observe/camera_result_page.dart
@@ -1,8 +1,11 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:kinoko_note/components/bottom_sheet.dart';
 import 'package:kinoko_note/pages/menu_page.dart';
+import 'package:path/path.dart';
 import '../../model/observation.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'dart:io';
 
 class Iobservation {
   late String name;
@@ -21,7 +24,6 @@ class _CameraResultPageState extends State<CameraResultPage> {
   late String imagePath;
   late AppDatabase db;
   late Iobservation observation;
-
   void setName(String name) {
     observation.name = name;
   }
@@ -30,19 +32,21 @@ class _CameraResultPageState extends State<CameraResultPage> {
     observation.imageName = imageName;
   }
 
+  int activeIndex = 0;
+
   @override
   void initState() {
     super.initState();
     imagePath = widget.imagePath;
     observation = new Iobservation();
     db = AppDatabase();
+    // _imagePath = (getExternalStorageDirectory());
   }
 
   Future<int> addObservation() async {
-    // print('保存データーーーーーーーーーーー');
-    // var test = await this.db.allObservationEntries;
-    // print(test);
-    return this.db.addObservation(this.observation.name, this.imagePath);
+    return this
+        .db
+        .addObservation(this.observation.name, basename(this.imagePath));
   }
 
   @override
@@ -55,12 +59,23 @@ class _CameraResultPageState extends State<CameraResultPage> {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Expanded(
-              child: Container(
-            width: 600,
-            height: 240,
-            child: Image.file(File(imagePath), fit: BoxFit.cover),
-          )),
+          CarouselSlider.builder(
+            options: CarouselOptions(
+                height: 400,
+                initialPage: 0,
+                viewportFraction: 1,
+                enlargeCenterPage: true,
+                onPageChanged: ((index, reason) => setState(() {
+                      activeIndex = index;
+                    }))),
+            itemCount: 4,
+            itemBuilder: (context, index, realIndex) {
+              final path = imagePath;
+              return buildImage(path, index);
+            },
+          ),
+          SizedBox(height: 20),
+          buildIndicator(),
           Align(
               alignment: Alignment.bottomCenter,
               child: ElevatedButton(
@@ -77,9 +92,6 @@ class _CameraResultPageState extends State<CameraResultPage> {
       )),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showForm(context),
-
-        // onPressed: () async {
-        // },
         child: Icon(Icons.edit_note),
       ),
     );
@@ -95,4 +107,25 @@ class _CameraResultPageState extends State<CameraResultPage> {
                 topLeft: Radius.circular(15), topRight: Radius.circular(15))),
         builder: (_) => builderBottomSheet(context, setName));
   }
+
+  Widget buildImage(path, index) => Container(
+        margin: EdgeInsets.symmetric(horizontal: 13),
+        color: Colors.grey,
+        child: Expanded(
+            child: Container(
+          width: 600,
+          height: 240,
+          child: Image.file(File(path), fit: BoxFit.cover),
+        )),
+      );
+
+  Widget buildIndicator() => AnimatedSmoothIndicator(
+        activeIndex: activeIndex,
+        count: 4,
+        effect: JumpingDotEffect(
+            dotHeight: 20,
+            dotWidth: 20,
+            activeDotColor: Colors.green,
+            dotColor: Colors.black12),
+      );
 }
